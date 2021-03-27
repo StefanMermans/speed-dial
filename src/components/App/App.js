409fbfb419f710d2429f7e55b32bafbfb443d936
Stefan Mermans <stefanmermans99@gmail.com>
2021-03-27 20:24:55 +0100

    fixed warning

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,13 +7,13 @@ import Clock from '../Clock/Clock';
 import styles from "./app.module.scss";
 
 function App() {
-  function onSignIn(googleUser) {
-    var profile = googleUser.getBasicProfile();
-    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    console.log('Name: ' + profile.getName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-  }
+  // function onSignIn(googleUser) {
+  //   var profile = googleUser.getBasicProfile();
+  //   console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  //   console.log('Name: ' + profile.getName());
+  //   console.log('Image URL: ' + profile.getImageUrl());
+  //   console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+  // }
 
   return (
       <div class={styles.appContainer} style={{
