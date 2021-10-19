b97cb0ae83608286b9366174f2366d53ed9ef064
Stefan Mermans <stefanmermans99@gmail.com>
2021-10-19 21:07:06 +0200

    service-worker test

---
+++ public/service-worker.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'MANUAL_CACHE';
+
+self.addEventListener('fetch', function (event) {
+  console.log('fetch', event);
+
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      if (response) {
+        return response;
+      } else {
+        return fetch(event.request).then(function (res) {
+          return caches.open(CACHE_NAME).then(function (cache) {
+            cache.put(event.request.url, res.clone());
+            return res;
+          });
+        });
+      }
+    }),
+  );
+});
