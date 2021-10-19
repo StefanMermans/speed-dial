1bdc69cc0d33f3e986e751b212c8eac05b9b4b79
Stefan Mermans <stefanmermans99@gmail.com>
2021-10-19 22:00:11 +0200

    ignore chrome extensions in service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,7 @@
 const CACHE_NAME = 'MANUAL_CACHE';
 
 const CACHE_REGEX = /.+\.(png|js|svg|jpg|jpeg)$/i;
+const NO_CACHE_REGEX = /(chrome-extension:\/\/)/i;
 
 function cacheFetch(event) {
   event.respondWith(
@@ -10,7 +11,10 @@ function cacheFetch(event) {
       } else {
         return fetch(event.request).then(function (res) {
           return caches.open(CACHE_NAME).then(function (cache) {
-            cache.put(event.request.url, res.clone());
+            cache.put(event.request, res.clone()).catch((e) => {
+              console.error(e, `URL: ${event.request.url}`);
+            });
+
             return res;
           });
         });
@@ -20,7 +24,13 @@ function cacheFetch(event) {
 }
 
 self.addEventListener('fetch', function (event) {
-  if (CACHE_REGEX.test(event.request.url)) {
+  if (
+    CACHE_REGEX.test(event.request.url) &&
+    !NO_CACHE_REGEX.test(event.request.url)
+  ) {
+    console.debug('CACHING: ', event.request.url);
     cacheFetch(event);
+  } else {
+    console.debug('NO-CACHE: ', event.request.url);
   }
 });
